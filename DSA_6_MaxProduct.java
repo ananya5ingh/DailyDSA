bc95d10e8c9b5c6f24033507bc75c4c4a6fa3081
ananya5ingh <ananyasingh2107@gmail.com>
2023-09-16 11:42:10 +0530

    Figured out String logic here

--- DSA_6_MaxProduct.java
+++ DSA_6_MaxProduct.java
@@ -7,14 +7,14 @@ How to find maximum product of two integers in the array
 where all elements are positive.
 
 int[] intArray = {10,20,30,40,50};
-maxProduct(intArray) // (40,50
+maxProduct(intArray) // (40,50)
  */
 
-
 public class DSA_6_MaxProduct {
-    public static int maxProduct(int[] intArray) {
+    public static String maxProduct(int[] intArray) {
         
         int maxProduct=0;
+        String pairs = "";
         for(int i=0; i<intArray.length; i++){
             for(int j=0; j<intArray.length; j++){
                 if(intArray[i] == intArray[j]){
@@ -24,15 +24,19 @@ public class DSA_6_MaxProduct {
                     int localProduct = intArray[i]*intArray[j];
                     if(localProduct>maxProduct){
                         maxProduct=localProduct;
+                        pairs = Integer.toString(intArray[i])+","+ Integer.toString(intArray[j]);
                     }
                 }
             }
         }
-        return maxProduct;
+        return pairs;
     }
+    // TC: O(n^2) // SC: 1
     
     public static void main(String[] args){
-        int[] arr = {10,20,30,40,50};
-        System.out.println(maxProduct(arr));
+        
+        int[] intArray = {10,20,30,40,50};
+        System.out.println(maxProduct(intArray));
     }
+    
 }
