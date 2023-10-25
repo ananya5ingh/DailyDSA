b58a9fd084d262cd2cdb504f89dd7d1b8eb35ca3
ananya5ingh <ananyasingh2107@gmail.com>
2023-10-26 03:11:57 +0530

    started again, day 1 questions

---
+++ DSA_37_NumberOfGoodPairs.java
@@ -0,0 +1,26 @@
+/*
+Day 1 - 25.10.2023
+Number Of Good Pairs
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+ */
+
+ class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int numOfGoodPairs = 0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] == nums[j]){
+                    numOfGoodPairs++;
+                }
+            }
+        }
+        return numOfGoodPairs;
+    }
+}
+
+
+public class Dsa_37_NumberOfGoodPairs {
+    
+}
